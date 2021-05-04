5dc3ee4a02016aff6a8f91e4df3b34f0f7e78a68
momoclouq <phamhoangminh050899@gmail.com>
2021-05-04 22:27:30 +0700

    Update on solution pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import Homepage from "./components/outerPages/Homepage";
 import {BrowserRouter as Router} from "react-router-dom";
 import Tos from "./components/outerPages/Tos";
 import GettingStarted from "./components/outerPages/GettingStarted";
+import Solution from "./components/outerPages/Solution";
 
 
 function App() {
   return (
     <Router>
-      <GettingStarted />
+      <Solution />
     </Router>
   );
 }
