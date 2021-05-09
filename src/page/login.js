9bfac5dfc12ab3efe4934bba6ea68591eaaa76c4
ainzworth <s3826227@rmit.edu.vn>
2021-05-09 11:38:53 +0700

    login and register ( MUI)

--- src/page/login.js
+++ src/page/login.js
@@ -1,30 +1,105 @@
 import React from "react";
 import "./login.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import GoogleButton from "react-google-button";
+import Paper from "@material-ui/core/Paper";
+import {
+  Card,
+  Container,
+  Avatar,
+  TextField,
+  Link,
+  withStyles,
+} from "@material-ui/core";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import { Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+
+const PurpleTypography = withStyles({
+  root: {
+    color: "#890596",
+    fontStyle: "solid",
+  },
+})(Typography);
+const LoginButton = withStyles({
+  root: {
+    color: "#890596",
+    borderColor: "#890596",
+  },
+})(Button);
+const useStyles = makeStyles((theme) => ({
+  root: { flexGrow: 1 },
+  card: {
+    padding: "30px",
+    height: "60vh",
+    width: "280px",
+    margin: "150px auto",
+  },
+  box: {
+    marin: "30px ",
+  },
+  body: {
+    background: "linear-gradient(to right,#9733ee,#da22ff)",
+    height: "100%",
+    width: "100%",
+    boxSizing: "border-box",
+    margin: "0",
+    padding: "0",
+    position: "absolute",
+  },
+}));
 const Login = () => {
+  const classes = useStyles();
   return (
-    <div className="container_2">
-      <h1>Trainee</h1>
-      <label className="label-class-username" htmlFor="user">
-        Username:
-      </label>
-      <input type="text" name="user" required />
-
-      <label className="label-class-password" htmlFor="pass">
-        Password:
-      </label>
-      <span className="psw">
-        Forgot <a href="// eslint-disable-next-line">Password?</a>
-      </span>
-      <input type="text" name="pass" required />
-
-      <button class="login" type="submit">
-        Login
-      </button>
-
-      <span className="rgs">
-        Don't have an account?{" "}
-        <a href="// eslint-disable-next-line">Create One</a>
-      </span>
+    <div className={classes.body}>
+      <Grid className={classes.root}>
+        <Card elevation={10} className={classes.card}>
+          <Box align="center">
+            <PurpleTypography variant="h5" className={classes.color}>
+              Trainee
+            </PurpleTypography>
+            <Box padding="20px">
+              <Typography variant="h3">Sign in</Typography>
+            </Box>
+            <TextField
+              margin="dense"
+              label="Username"
+              placeholder="Enter username"
+              fullWidth
+              required
+            />
+            <TextField
+              margin="dense"
+              label="Password"
+              placeholder="Enter passwor"
+              fullWidth
+              required
+            />
+            <Box p={2}>
+              <LoginButton variant="outlined" href="#outlined-buttons">
+                Login
+              </LoginButton>
+            </Box>
+            <Box pt={1}>
+              <Link href="">Forgot Password?</Link>
+            </Box>
+            <Box p={1}>
+              Haven't had an account ?
+              <Link href="" margin="dense">
+                Register now
+              </Link>
+            </Box>
+            <GoogleButton
+              onClick={() => {
+                console.log("Google button clicked");
+              }}
+            />
+          </Box>
+        </Card>
+      </Grid>
     </div>
   );
 };
--- src/page/register.js
+++ src/page/register.js
@@ -1,33 +1,126 @@
 import React from "react";
-import "./register.css";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  Container,
+  Avatar,
+  TextField,
+  Link,
+  withStyles,
+  Box,
+  Button,
+  Typography,
+  Paper,
+  Grid,
+} from "@material-ui/core";
+//icon
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import KeyboardReturnIcon from "@material-ui/icons/KeyboardReturn";
+import GoogleButton from "react-google-button";
 
+const PurpleTypography = withStyles({
+  root: {
+    color: "#890596",
+    fontStyle: "solid",
+  },
+})(Typography);
+const LoginButton = withStyles({
+  root: {
+    color: "white",
+    borderColor: "#890596",
+    backgroundColor: "#890596",
+    transition: "all 0.2s ease-in-out",
+    "&:hover": {
+      paddingRight: "30px",
+      paddingLeft: "30px",
+      backgroundColor: "#890596",
+      color: "rgba(255, 255, 255, 1)",
+      boxShadow: "0 5px 15px rgba(145, 92, 182, .4)",
+    },
+  },
+})(Button);
+const useStyles = makeStyles((theme) => ({
+  root: { flexGrow: 1 },
+  card: {
+    padding: "30px",
+    height: "fit-content",
+    width: "280px",
+    margin: "110px auto",
+  },
+  box: {
+    marin: "30px ",
+  },
+  body: {
+    background: "linear-gradient(to right,#9733ee,#da22ff)",
+    height: "100%",
+    width: "100%",
+    boxSizing: "border-box",
+    margin: "0",
+    padding: "0",
+    position: "absolute",
+  },
+  backButton: {
+    position: "absolute",
+    backgroundColor: "white",
+    border: "none",
+    transition: "all 0.2s ease-in-out",
+    fontSize: "24px",
+    "&:hover": {
+      position: "absolute",
+      fontSize: "30px",
+    },
+  },
+}));
 const Register = () => {
+  const classes = useStyles();
   return (
-    <div className="container">
-      <div className="container_2">
-        <h1>Register Form</h1>
-
-        <h2>Trainne</h2>
-        <hr style={{ clear: "both" }} />
-      </div>
-      <div className="container_1">
-        <div className="con_1">
-          <label htmlFor="">First Name:</label>
-          <label htmlFor="">Last Name:</label>
-          <label htmlFor="">Email:</label>
-          <label htmlFor="">Password:</label>
-          <label htmlFor="">Confirm Password:</label>
-        </div>
-
-        <div className="con_2">
-          <input type="text" name="fName" required />
-          <input type="text" name="LName" required />
-          <input type="text" name="Email" required />
-          <input type="text" name="Password" required />
-          <input type="text" name="C_password" required />
-        </div>
-      </div>
-      <button>Register</button>
+    <div className={classes.body}>
+      <Card elevation={10} className={classes.card}>
+        <button className={classes.backButton}>
+          <KeyboardReturnIcon fontSize="inherit"></KeyboardReturnIcon>
+        </button>
+        <Box align="center" pb={2}>
+          <PurpleTypography variant="h5" className={classes.color}>
+            Trainee
+          </PurpleTypography>
+          <Box padding="20px">
+            <Typography variant="h4">Sign Up Form</Typography>
+          </Box>
+          <TextField
+            margin="dense"
+            label="Username"
+            placeholder="Enter username"
+            fullWidth
+            required
+          />
+          <TextField
+            margin="dense"
+            label="Password"
+            placeholder="Enter password"
+            fullWidth
+            required
+          />
+          <TextField
+            margin="dense"
+            label="Confirm Password"
+            placeholder="Confirm Password"
+            fullWidth
+            required
+          />
+          <TextField
+            margin="dense"
+            label="Email"
+            placeholder="Email"
+            fullWidth
+            required
+          />
+        </Box>
+        <Box textAlign="center">
+          <LoginButton variant="contained" href="#outlined-buttons">
+            Register
+          </LoginButton>
+        </Box>
+      </Card>
     </div>
   );
 };
